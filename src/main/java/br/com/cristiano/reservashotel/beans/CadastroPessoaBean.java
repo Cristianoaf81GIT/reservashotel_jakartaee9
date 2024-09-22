f977e8b78236fa08e4912beb22518ca86de6ba6b
Cristiano Alexandre de Faria <cristianoaf81@gmail.com>
2024-09-22 20:49:25 -0300

    feat(15): practicing jsf part II
    Closes#main

--- src/main/java/br/com/cristiano/reservashotel/beans/CadastroPessoaBean.java
+++ src/main/java/br/com/cristiano/reservashotel/beans/CadastroPessoaBean.java
@@ -2,8 +2,9 @@ package br.com.cristiano.reservashotel.beans;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.List;
 import java.util.Objects;
+import br.com.cristiano.reservashotel.enums.Sexo;
 import br.com.cristiano.reservashotel.models.Pessoa;
 import br.com.cristiano.reservashotel.models.PessoaFisica;
 import jakarta.enterprise.context.SessionScoped;
@@ -16,12 +17,22 @@ public class CadastroPessoaBean implements Serializable {
     private static final long serialVersionUID = -7600674761430266600L;
 
     private Pessoa pessoalSelecionada;
-    private Collection<Pessoa> lista;
-
+    private List<Pessoa> lista;
 
     public CadastroPessoaBean() {
         this.pessoalSelecionada = new PessoaFisica();
         this.lista = new ArrayList<Pessoa>();
+        for (int x = 0; x < 10; x++){
+            PessoaFisica p = new PessoaFisica();
+            p.setNome(String.format("fulano %02d", x));
+            p.setEmail(String.format("fulano%02d@email.com", x));
+            p.setTelefone(String.format("999.88%2d", x));
+            p.setRg(String.format("0000000-%02d",x));
+            p.setCpf(String.format("000-000-111-sp-%02d",x));            
+            p.setSexo(Sexo.M);
+            p.setCodigo(x);
+            this.lista.add(p);
+        }
     }
 
 
@@ -33,11 +44,11 @@ public class CadastroPessoaBean implements Serializable {
         this.pessoalSelecionada = p;
     }
 
-    public Collection<Pessoa> getList() {
+    public List<Pessoa> getLista() {
         return this.lista;
     }
 
-    public void setList(Collection<Pessoa> lista) {
+    public void setLista(List<Pessoa> lista) {
         this.lista = lista;
     }
 
@@ -68,6 +79,14 @@ public class CadastroPessoaBean implements Serializable {
         System.out.println("Nome: " + this.pessoalSelecionada.getNome());
         System.out.println("Email: " + this.pessoalSelecionada.getEmail());
         System.out.println("Telefone: " + this.pessoalSelecionada.getTelefone());
+        if (
+            this.pessoalSelecionada.getNome().length() > 0 && 
+            !this.lista.contains(this.pessoalSelecionada)
+        ) {
+            int nextId = this.lista.size();
+            this.pessoalSelecionada.setCodigo(nextId);
+            this.lista.add(this.pessoalSelecionada);
+        }
     }
 
 }
