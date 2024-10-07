a66446a3e9534180165ef19e5e33bdbf2708e19d
Cristiano Alexandre de Faria <cristianoaf81@gmail.com>
2024-10-07 15:25:36 -0300

    feat(18): view components 1
    Closes#main

--- src/main/java/br/com/cristiano/reservashotel/beans/MeuPrimeiroBean.java
+++ src/main/java/br/com/cristiano/reservashotel/beans/MeuPrimeiroBean.java
@@ -1,6 +1,9 @@
 package br.com.cristiano.reservashotel.beans;
 
 import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.component.UIComponent;
+import jakarta.faces.component.UIInput;
+import jakarta.faces.event.AjaxBehaviorEvent;
 import jakarta.inject.Named;
 
 @RequestScoped
@@ -8,8 +11,32 @@ import jakarta.inject.Named;
 public class MeuPrimeiroBean {
     
     private String ola = "Olá do java Bean";
+    private boolean mostrar = false;
 
     public String getOla() {
         return this.ola;
+    }
+
+
+    public boolean getMostrar() {
+        return this.mostrar;
+    }
+
+    public void setMostrar(boolean mostrar) {
+        this.mostrar = mostrar;
+    }
+    
+    public void addMessage(AjaxBehaviorEvent event) {
+        System.out.println("executing...");
+        UIComponent component = event.getComponent();
+        System.out.println("event " + component); 
+        if (component instanceof UIInput) {
+            System.out.println("if");
+            UIInput inputComponent = (UIInput) component;
+            Boolean value = (Boolean) inputComponent.getValue();
+            String summary = value ? "Checked" : "Unchecked";
+            this.mostrar = true;
+            System.out.println(summary);
+        }
     }
 }
